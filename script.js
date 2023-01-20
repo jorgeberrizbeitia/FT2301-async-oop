83628653da8ebe684b73bd6456a2adbf97911e06
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2023-01-20 14:52:42 +0100

    methods done

--- script.js
+++ script.js
@@ -1,73 +1,61 @@
-console.log("probando!")
+console.log("probando!");
 
 // CALLBACKS
 
 function decirHola() {
-  console.log("Hola Mundo!")
+  console.log("Hola Mundo!");
 } // ref 1234
 
 // decirHola()
 function decirAdios() {
-  console.log("Adios Mundo!")
+  console.log("Adios Mundo!");
 }
 
-
-
-function funcionQueAceptaOtraFuncion( callback ) {
+function funcionQueAceptaOtraFuncion(callback) {
   // let unaFuncion = decirHola // ref 1234
 
   // decirHola()
-  callback()
+  callback();
 }
 
+funcionQueAceptaOtraFuncion(decirHola);
+funcionQueAceptaOtraFuncion(decirAdios);
 
-funcionQueAceptaOtraFuncion( decirHola )
-funcionQueAceptaOtraFuncion( decirAdios )
-
-
-// 1. 
+// 1.
 
 // forEach => recibe otra funcion
 
 function printSomething() {
-  console.log("ejecutando algo...")
+  console.log("ejecutando algo...");
 }
 
-let numArr = [2, 10, 23]
+let numArr = [2, 10, 23];
 
-numArr.forEach( printSomething )
+numArr.forEach(printSomething);
 
-numArr.forEach( function() {
-  console.log("ejecutando algo con funcion anonima")
-} )
+numArr.forEach(function () {
+  console.log("ejecutando algo con funcion anonima");
+});
 
-numArr.forEach( () => {
-  console.log("ejecutando algo con funcion anonima de tipo flecha")
-} )
+numArr.forEach(() => {
+  console.log("ejecutando algo con funcion anonima de tipo flecha");
+});
 
 const arrowFunction = () => {
   //...
-}
-
-
-
+};
 
 // 2. muchos desarrolladores, crear logicas minimizando redundancia de codigo usando callbacks.
-
-
-
 
 // DA METODOS QUE SIMULAN UN COMPORTAMIENTO ASINCRONO
 
 // JS es un lenguaje sincrono con comportamientos asincronos
-
 
 // setTimeout
 
 // 2 argumentos.
 // 1. una funcion de callback a invocar
 // 2. una cantidad de tiempo en ms que debe esperar antes de ejecutar la funcion
-
 
 // setTimeout( () => {
 //   console.log("ha pasado cierto tiempo real") // 3
@@ -79,19 +67,16 @@ const arrowFunction = () => {
 //   console.log("a los 0 segundos") // 2
 // }, 0 )
 
-console.log("luego de los setTimeout") // 1
+console.log("luego de los setTimeout"); // 1
 
 // JS primero resuelve TODOS las ejecuciones sincronas.
 // Y luego, resuelve TODAS las ejecuciones asincronas
-
 
 // setInterval => crea una ejecucion constante
 
 // setInterval( () => {
 //   console.log("ejecutando hasta el fin")
 // }, 1500 )
-
-
 
 // como controlamos un intervalo
 
@@ -104,7 +89,7 @@ console.log("luego de los setTimeout") // 1
 //   control++
 
 //   console.log("intervalo controlado", control)
-  
+
 //   if (control >= 10) {
 //     // deten el intervalo con clearInterval
 //     clearInterval(intervalId) // debemos pasarle con argumento el id de el intervalo a detener
@@ -112,17 +97,13 @@ console.log("luego de los setTimeout") // 1
 
 // }, 1000);
 
-
-
-
 // ejemplo de una funcion que retorna un valor y lo guardamos en una variable
 function fullName(name, lastName) {
-  return name + " " + lastName
+  return name + " " + lastName;
 }
 
-let someName = fullName("Bob", "Mcintosh")
-console.log(someName) // "Bob Mcintosh"
-
+let someName = fullName("Bob", "Mcintosh");
+console.log(someName); // "Bob Mcintosh"
 
 let control = 0;
 
@@ -134,37 +115,80 @@ let control = 0;
 //   clearInterval(intId)
 // }, 4000 )
 
-
-
-
 function blast() {
-
   let timer = 0;
 
-  let interId = setInterval( () => {
-    timer++
+  let interId = setInterval(() => {
+    timer++;
     // console.log("timer", timer)
     if (timer % 3 === 0 && timer % 5 === 0) {
-      console.log("BOOM BANG!!")
+      console.log("BOOM BANG!!");
     } else if (timer % 3 === 0) {
-      console.log("BOOM!!")
+      console.log("BOOM!!");
     } else if (timer % 5 === 0) {
-      console.log("BANG!!")
+      console.log("BANG!!");
     } else {
-      console.log(timer)
+      console.log(timer);
     }
 
     // if (timer >= 15) {
     //   clearInterval(interId)
     // }
-  }, 1000 )
+  }, 1000);
 
-  setTimeout( () => {
-    clearInterval(interId)
-  }, 15000 )
-
+  setTimeout(() => {
+    clearInterval(interId);
+  }, 15000);
 }
 
+// blast()
 
-blast()
+// METODOS
 
+let person = {
+  name: "alicia",
+  place: "Pais de las Maravillas",
+  friends: ["Sombrerero", "Gato Chesire", "Humty Dumpty", "Conejo Blanco"],
+  sayHello() {
+    
+    // console.log(this);
+    // return `Hola, mi nombre es ${person.name} y soy de ${person.place}`;
+    return `Hola, mi nombre es ${this.name} y soy de ${this.place}`;
+  },
+  personSize() {
+    // de manera aleatoria me arroje dos posibles mensajes:
+    // 1. return "Alicia es pequeña"; 50%
+    // 2. return "Alicia es grande"; 50%
+    let randomNum = Math.random() * 10 // 0 - 0.999999
+    let randomNumInt = Math.floor( randomNum )
+    console.log(randomNumInt)
+    if ( randomNumInt <= 4 ) {
+      return `${this.name} es pequeña`
+    } else {
+      return `${this.name} es grande`
+    }
+  },
+  felizNoCumpleaños() {
+    // busque un amigo aleatorio
+    // le desee feliz no cumpleaños
+    let randomNum = Math.random() * this.friends.length
+    let randomNumInt = Math.floor( randomNum )
+
+    return `Feliz feliz no cumpleaños a ${this.friends[randomNumInt]}`
+  }
+};
+
+console.log(person.name);
+// console.log( person.sayHello )
+
+person.name = "Alice";
+console.log(person.sayHello());
+
+// "this" apunta a el objeto cuando se usa dentro de propiedades o metodos de ese objeto
+
+// console.log(this);
+// this en el scope global apunta a todo la base del codigo
+
+
+console.log(person.personSize())
+console.log(person.felizNoCumpleaños())
